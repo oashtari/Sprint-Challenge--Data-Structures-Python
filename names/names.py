15b51a3154730858311fca8c18a5bba46d834450
Omid Ashtari <oashtari@gmail.com>
2020-09-27 20:41:23 -0700

    finished udpated names query

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BinarySearchTree
 
 start_time = time.time()
 
@@ -20,10 +21,31 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-for name_1 in names_1:
-    if name_1 in names_2:
-        print('name', name_1)
-        duplicates.append(name_1)
+# FASTER SOLUTION, but I need to use a data structure :facepalm:, almost did not see that
+# for name_1 in names_1:
+#     if name_1 in names_2:
+#         print('name', name_1)
+#         duplicates.append(name_1)
+
+# BST solution
+new_tree = BinarySearchTree(names_1[0])
+
+for name in range(1, len(names_1)):
+    new_tree.insert(names_1[name])
+
+for name in names_2:
+    if new_tree.contains(name):
+        duplicates.append(name)
+
+# could not figure out how to compare to BSTs :(
+
+
+# other_tree = BinarySearchTree(names_2[0])
+
+# for name in range(1, len(names_2)):
+#     other_tree.insert(names_2[name])
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
